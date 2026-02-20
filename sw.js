bf45f7dcef814ad739b0c07140242c89f6ef168c
7D4vi5 <119212424+Calmingly@users.noreply.github.com>
2026-02-20 18:53:19 +0000

    Halve routine images and tune hero image section

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "m0b1li7y-v11";
+const VERSION = "m0b1li7y-v12";
 const APP_SHELL = `app-shell-${VERSION}`;
 const REMOTE_IMAGES = `remote-images-${VERSION}`;
 const MAX_REMOTE_IMAGE_ENTRIES = 40;
