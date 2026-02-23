c9e4c92dc58b086d31aed0d2394c657f47ab934d
7D4vi5 <119212424+Calmingly@users.noreply.github.com>
2026-02-23 14:11:28 +0000

    feat: redesign app with orbital slate layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "m0b1li7y-v16";
+const VERSION = "m0b1li7y-v18";
 const APP_SHELL = `app-shell-${VERSION}`;
 const REMOTE_IMAGES = `remote-images-${VERSION}`;
 const MAX_REMOTE_IMAGE_ENTRIES = 40;
@@ -7,6 +7,8 @@ const CORE_ASSETS = [
   "./index.html",
   "./styles.css",
   "./styles.css?v=20260223r1",
+  "./styles.css?v=20260223r2",
+  "./styles.css?v=20260223r3",
   "./src/app.js",
   "./src/routineImages.js",
   "./manifest.webmanifest",
